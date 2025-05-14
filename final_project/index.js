305e92e430f0aa0fb55148d66e8d4cf80871871e
irfan-alyy <irfanserboon@gmail.com>
2025-05-14 17:45:20 -0400

    final proejct started

--- final_project/index.js
+++ final_project/index.js
@@ -4,13 +4,28 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+const secret= "irfanaly"
 const app = express();
 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
 app.use("/customer/auth/*", function auth(req,res,next){
+
+    if(req.session.authorization){
+        const token= req.session.authorization["accessToken"]
+        jwt.verify(token,secret, (err, user)=>{
+            if(!err){
+                req.user=user
+                next()
+            }
+            else{
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        } )
+    }else{
+       return res.status(403).json({ message: "User not logged in" })
+    }
 //Write the authenication mechanism here
 });
  
